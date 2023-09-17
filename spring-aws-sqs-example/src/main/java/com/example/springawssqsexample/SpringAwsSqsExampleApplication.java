b064e68013b4a8093d584144883d1fcbe5aa0472
hamza <hamza.nassour13@gmail.com>
2023-09-17 13:18:30 +0100

    spring cloud aws sqs examples

---
+++ spring-aws-sqs-example/src/main/java/com/example/springawssqsexample/SpringAwsSqsExampleApplication.java
@@ -0,0 +1,25 @@
+package com.example.springawssqsexample;
+
+import com.example.springawssqsexample.service.MessageSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SpringAwsSqsExampleApplication implements CommandLineRunner {
+
+    @Autowired
+    private MessageSender messageSender;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsSqsExampleApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+       //  messageSender.sendMessage("Hello World");
+    }
+}
---
+++ spring-aws-sqs-example/src/main/java/com/example/springawssqsexample/config/SqsConfig.java
@@ -0,0 +1,78 @@
+package com.example.springawssqsexample.config;
+
+import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
+import io.awspring.cloud.sqs.listener.acknowledgement.AcknowledgementResultCallback;
+import io.awspring.cloud.sqs.listener.acknowledgement.handler.AcknowledgementMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.util.Collection;
+
+
+@Configuration
+public class SqsConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SqsConfig.class);
+
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+
+    /*
+    @Bean
+    SqsAsyncClient sqsAsyncClient(){
+        return SqsAsyncClient
+                .builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider
+                        .create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+        // add more Options
+    }
+
+    @Bean
+    public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient){
+        return SqsTemplate.builder().sqsAsyncClient(sqsAsyncClient).build();
+    }*/
+
+
+
+
+    @Bean
+    SqsMessageListenerContainerFactory<Object> defaultSqsListenerContainerFactory(SqsAsyncClient sqsAsyncClient) {
+        return SqsMessageListenerContainerFactory.builder()
+                .configure(options -> options.acknowledgementMode(AcknowledgementMode.MANUAL)
+                        .acknowledgementInterval(Duration.ofSeconds(3)) // NOTE: With acknowledgementInterval 3 seconds,
+                        .acknowledgementThreshold(0)
+                )
+                .acknowledgementResultCallback(new AckResultCallback()).sqsAsyncClient(sqsAsyncClient).build();
+    }
+
+
+    static class AckResultCallback implements AcknowledgementResultCallback<Object> {
+        @Override
+        public void onSuccess(Collection<Message<Object>> messages) {
+            LOGGER.info("Ack with success at {}", OffsetDateTime.now());
+        }
+
+        @Override
+        public void onFailure(Collection<Message<Object>> messages, Throwable t) {
+            LOGGER.error("Ack with fail", t);
+        }
+    }
+
+}
